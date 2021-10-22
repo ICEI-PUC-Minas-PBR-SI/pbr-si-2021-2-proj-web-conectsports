18b7e54092fb5981eae6340d4686820c7b14744c
Bia <75254752+biavitoria@users.noreply.github.com>
2021-10-22 12:22:14 -0300

    Add db fake

---
+++ Codigo/dbfake.js
@@ -0,0 +1,17 @@
+var db = {
+    dados: [
+        {
+            data: '15/11/2021',
+            hora: '14:00',
+            local: 'Parque das Águas'
+        }, {
+            data: '10/10/2021',
+            hora: '16:00',
+            local: 'Av. Teresa Cristina'
+        }, {
+            data: '4/12/2021',
+            hora: '10:00',
+            local: 'Praça do Papa'            
+        }
+    ]
+}
